7b4a6d5ee7364b2a430e5de274a95dd379491810
Rohith Kumar Jayana <jayanarohith7@gmail.com>
2021-07-31 17:21:31 +0530

    final

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from werkzeug.utils import redirect
 from myanimelyrics import search_lyrics
 import urllib.request
 import re
+import random
 # from flask.wrappers import Request
 
 app = Flask(__name__)
@@ -32,6 +33,7 @@ def index():
             jply = ''
             # jply += searchly_jp.split('\n')[0]+' '+searchly_jp.split('\n')[1]+' '+searchly_jp.split('\n')[2]+' '+searchly_jp.split('\n')[3]+' '
             res = searchly_jp.split('\n')
+            # print(res)
             for line in range(0,4):
                 if line != '\n':
                     jply = jply + res[line]
@@ -44,10 +46,10 @@ def index():
             jply = jply.replace("\r", "+").strip()
             print(jply)
             html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + jply)
-            # print(html.read().decode())
+            # # print(html.read().decode())
             video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
             print(video_ids)
-            firstvideo_id = "https://www.youtube.com/embed/" + video_ids[0]
+            firstvideo_id = "https://www.youtube.com/embed/" + random.choice(video_ids)
             # return redirect('/')
             return render_template("index.html",ly_jp = ly_jp, ly_en = ly_en, firstvideo_id = firstvideo_id)
         else:
